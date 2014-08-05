5fb7a9edc095a6525521cd581efcaaf2f88b5da9
Joey Sacchini <joey.sacchini264@gmail.com>
2014-08-05 01:09:35 -0400

    lol wut

--- TBNRSG/src/main/java/net/tbnr/dev/sg/SurvivalGames.java
+++ TBNRSG/src/main/java/net/tbnr/dev/sg/SurvivalGames.java
@@ -3,6 +3,5 @@ package net.tbnr.dev.sg;
 import net.cogzmc.core.modular.ModularPlugin;
 import net.cogzmc.core.modular.ModuleMeta;
 
-@ModuleMeta(name = )
 public class SurvivalGames extends ModularPlugin {
 }
