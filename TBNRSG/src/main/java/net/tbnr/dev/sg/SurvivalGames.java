67d2a355ae82c903191ed197d42b461ddbeccc91
Joey Sacchini <joey.sacchini264@gmail.com>
2014-08-05 01:08:18 -0400

    updates to provide working builds

---
+++ TBNRSG/src/main/java/net/tbnr/dev/sg/SurvivalGames.java
@@ -0,0 +1,8 @@
+package net.tbnr.dev.sg;
+
+import net.cogzmc.core.modular.ModularPlugin;
+import net.cogzmc.core.modular.ModuleMeta;
+
+@ModuleMeta(name = )
+public class SurvivalGames extends ModularPlugin {
+}
